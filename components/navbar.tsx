77a2951e4a3557f34fd2e44ec40638b72084cb8b
ecdns <clementpavot78@gmail.com>
2025-05-05 22:22:12 +0200

    navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,6 @@
+"use client"; 
+
+import { useState } from "react"; 
 import {
   Navbar as HeroUINavbar,
   NavbarContent,
@@ -8,132 +11,97 @@ import {
   NavbarMenuItem,
 } from "@heroui/navbar";
 import { Button } from "@heroui/button";
-import { Kbd } from "@heroui/kbd";
 import { Link } from "@heroui/link";
-import { Input } from "@heroui/input";
-import { link as linkStyles } from "@heroui/theme";
 import NextLink from "next/link";
-import clsx from "clsx";
-
-import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
-import {
-  TwitterIcon,
-  GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
-  Logo,
-} from "@/components/icons";
+import { Image } from "@heroui/image";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+  const [isAuthenticated, setIsAuthenticated] = useState(false); 
 
   return (
-    <HeroUINavbar maxWidth="xl" position="sticky">
+    <HeroUINavbar className="bg-[#f3ede7]" maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+          <Image src="/rr.png" alt="Logo" height={40} />
           </NextLink>
         </NavbarBrand>
-        <div className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium"
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </div>
       </NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.twitter} title="Twitter">
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.discord} title="Discord">
-            <DiscordIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.github} title="GitHub">
-            <GithubIcon className="text-default-500" />
-          </Link>
-          <ThemeSwitch />
-        </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          <Button
-            isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
-            variant="flat"
-          >
-            Sponsor
-          </Button>
+      <NavbarContent className="hidden sm:flex sm:basis-full" justify="end">
+        <NavbarItem className="hidden md:flex justify-end w-full gap-3">
+          {isAuthenticated ? (
+            <>
+              <Button as={Link} className="text-sm font-normal text-default-600 bg-default-100" href="/ressources" variant="flat">
+                Ressources
+              </Button>
+              <Button as={Link} className="text-sm font-normal text-default-600 bg-default-100" href="/forum" variant="flat">
+                Forum
+              </Button>
+              <Button as={Link} className="text-sm font-normal text-default-600 bg-default-100" href="/aide" variant="flat">
+                Aide
+              </Button>
+              <Button as={Link} className="text-sm font-normal text-white bg-[#0a4267]" href="/profil">
+                Profil
+              </Button>
+            </>
+          ) : (
+            // Si l'utilisateur n'est pas authentifié
+            <>
+              <Button as={Link} className="text-sm font-normal text-default-600 bg-default-100" href="/home" variant="flat">
+                Accueil
+              </Button>
+              <Button as={Link} className="text-sm font-normal text-default-600 bg-default-100" href="/ressources" variant="flat">
+                Ressources
+              </Button>
+              <Button as={Link} className="text-sm font-normal text-default-600 bg-default-100" href="/aide" variant="flat">
+                Aide
+              </Button>
+              <Button as={Link} className="text-sm font-normal text-white bg-[#0a4267]" href="/login">
+                Connexion
+              </Button>
+            </>
+          )}
         </NavbarItem>
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
-        <NavbarMenuToggle />
+        <NavbarMenuToggle className="text-[#0a4267]" />
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
+        <div className="mx-4 mt-2 flex flex-col">
+          {isAuthenticated ? (
+            <>
+              <NavbarMenuItem>
+                <Link href="/ressources" size="lg">Ressources</Link>
+              </NavbarMenuItem>
+              <NavbarMenuItem>
+                <Link href="/forum" size="lg">Forum</Link>
+              </NavbarMenuItem>
+              <NavbarMenuItem>
+                <Link href="/aide" size="lg">Aide</Link>
+              </NavbarMenuItem>
+              <NavbarMenuItem>
+                <Link href="/profil" size="lg" color="primary">Profil</Link>
+              </NavbarMenuItem>
+            </>
+          ) : (
+            <>
+              <NavbarMenuItem>
+                <Link href="/home" size="lg">Accueil</Link>
+              </NavbarMenuItem>
+              <NavbarMenuItem>
+                <Link href="/ressources" size="lg">Ressources</Link>
+              </NavbarMenuItem>
+              <NavbarMenuItem>
+                <Link href="/aide" size="lg">Aide</Link>
+              </NavbarMenuItem>
+              <NavbarMenuItem>
+                <Link href="/login" size="lg" color="primary">Connexion</Link>
+              </NavbarMenuItem>
+            </>
+          )}
         </div>
       </NavbarMenu>
     </HeroUINavbar>
